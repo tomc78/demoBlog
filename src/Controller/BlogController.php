dc62596ea96c8c0acfe961ef1e59a0d963d06937
tomc78 <thomas.cres78@live.fr>
2020-05-25 14:41:16 +0200

    première version blog SYMFONY

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    // Un commetaire qui commence par @ est une annotation très iportante, Symfony explique que lorsqu'on lancera wwww.monsite
+    //on fera appel a la méthode index()
+    // Pas besoin de préciser templates/blog/index.html.twig, Symfony sait où se trouve les fichiers templates de rendu .
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /** 
+     * @Route("/", name="home") 
+    */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig');
+    }
+
+
+}
+
+
