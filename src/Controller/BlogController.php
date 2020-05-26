7d35bccb02a943e7af5fa6bdadfce489312ff063
tomc78 <thomas.cres78@live.fr>
2020-05-26 17:23:17 +0200

    création entité - table SQL +DOCTRINE select

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,7 +16,27 @@ class BlogController extends AbstractController
      * @Route("/blog", name="blog")
      */
     public function index()
-    {
+    {   
+        /*
+            Pour selectionner des données en BDD, nous avons besoin de la classe Repository de la classe Article
+            Une classe Repository permet uniquement de selectionner des données en BDD (requete SQL SELECT)
+            On a besoin de l'ORM DOCTRINE pour faire la relation entre la BDD et notre application (getDoctrine())
+            getRepository() : méthode issue de l'objet DOCTRINE qui permet d'importer une classe Repository (SELECT)
+
+            $repo est un objet issu de la classe ArticleRepository, cette contient des méthodes prédéfinies par SYMFONY permettant de selectionner des données en BDD (find, findBy, findOneBy, findAll)
+
+            dump() : équivalent de var_dump(), permet d'observer le resultat de la requete de selection en bas de la page dans la barre administrative (cible à droite)
+        */
+
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repo->findAll();
+        // findAll() est une méthode issue de la classe ArticleRepository qui permet de selectionner l'ensemble de la table (similaire à SELECT * FROM article)
+
+        
+
+        dump($articles);
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
@@ -33,7 +54,21 @@ class BlogController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/blog/45", name="blog_show")
+     */
+    public function show()
+    {
+        return $this->render('blog/show.html.twig');
+    }
 
+    /**
+     * @Route("/blog/46", name="blog_create")
+     */
+    public function create()
+    {
+        return $this->render('blog/create.html.twig');
+    }
 }
 
 
