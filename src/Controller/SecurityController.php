c501b66a4e88dc3d2b949ac14817971ec5238c83
Thomas CRESPEL <thomas.cres78@live.fr>
2020-06-04 17:28:30 +0200

    ajout backOffice et gestion des Roles

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,8 +6,10 @@ use App\Entity\User;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
@@ -45,9 +47,15 @@ class SecurityController extends AbstractController
     /**
      * @Route("/connexion",name="security_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils):Response
     {
-        return $this->render('security/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig' , [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 
     /**
